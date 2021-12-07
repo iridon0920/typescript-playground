7cab8dcedba75e2e30eabfe233bfbec4a43a673e
iridon0920 <iridon0920@gmail.com>
2021-12-07 22:04:45 +0900

    unknown型、 boolean型の特性

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-console.log('helloaaaa')
-console.log('aaaaaabb')
+let a: unknown = 30 //unknown
+let b = a === 123 // boolean
+let C = a + 10 // error
+if (typeof a === 'number') {
+  let d = a + 10 // number
+}
+
+let a = true // boolean
+var B = false // boolean
+const c = true // true型
+let d: boolean = true // boolean
+let e: true = true // true型
+let f: true = false // error
